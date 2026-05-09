73bdedc7a2e59a7471f4894e060ce4bff7b4f517
Ubuntu <ubuntu@ip-172-31-34-103.ap-south-1.compute.internal>
2026-05-09 10:58:22 +0000

    Add clean Kubernetes manifests

---
+++ eks-terraform/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
+
+  cluster_name    = "enterprise-eks"
+  cluster_version = "1.30"
+
+  subnet_ids = module.vpc.private_subnets
+vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+}
